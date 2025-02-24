e18e3669a646cc0bf68e7962233184d691ec8c4f
Ghanashyam Aryal <anjanaryal13@gmail.com>
2025-02-24 16:07:26 +0545

    user functionality

--- controller/Authentication.controller.js
+++ controller/Authentication.controller.js
@@ -1,35 +1,35 @@
-
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../model/user.model.js";
 
-
-const Authentication =  async (req, res) => {
-    const { email, password  } = req.body;
+const Authentication = async (req, res) => {
+    const { email, password } = req.body;
 
     if (!email || !password) {
         return res.status(400).json({ message: "Email and password are required." });
     }
 
     try {
-        
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: "User not found." });
         }
 
-       
         const isPasswordMatch = await bcrypt.compare(password, user.password);
         if (!isPasswordMatch) {
             return res.status(401).json({ message: "Invalid email or password." });
         }
 
-       
         const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
             expiresIn: "1h",
         });
 
-        res.status(200).json({ message: "Login successful.", token,role: user.role });
+        res.status(200).json({
+            message: "Login successful.",
+            token,
+            role: user.role,
+            username: user.username
+        });
     } catch (error) {
         console.error("Error during login:", error);
         res.status(500).json({ message: "Internal server error." });
