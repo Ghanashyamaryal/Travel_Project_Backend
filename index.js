48b458f39e7f33538d02b99ef800e92b62af7755
Ghanashyam Aryal <anjanaryal13@gmail.com>
2025-08-21 16:15:53 +0545

    feat: add cron jobs

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';
 import Connect from './db/index.js';
+import cron from 'node-cron';
 
 dotenv.config();
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
   Connect();
+
+  cron.schedule('0 * * * *', () => {
+    console.log('Hourly task running:', new Date());
+  });
 });
